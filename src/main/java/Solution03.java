41c9d14dde76bfaa463b349fe638ee9803d3eb94
jad714 <josh01100100@gmail.com>
2021-09-02 14:43:44 -0400

    Initial commit

---
+++ src/main/java/Solution03.java
@@ -0,0 +1,27 @@
+import java.util.Scanner;
+
+/*
+ *  UCF COP3330 Fall 2021 Assignment 1 Solution
+ *  Copyright 2021 Joshua Davis
+ */
+
+public class Solution03 {
+    /* Begin pseudocode */
+    // Create scanner object.
+    // Create variables for storing two strings (quote and attribution).
+    // Prompt the user for quote.
+    // Prompt the user for attribution.
+    // Print statement (one line) of quote.
+    /* End pseudocode */
+
+    public static void main(String[] args){
+        Scanner userInput = new Scanner(System.in);
+        String quote;
+        String attribution;
+        System.out.print("What is the quote? ");
+        quote = userInput.nextLine();
+        System.out.print("\nWho said it? ");
+        attribution = userInput.nextLine();
+        System.out.println(attribution + " says, \"" + quote + "\"");
+    }
+}
